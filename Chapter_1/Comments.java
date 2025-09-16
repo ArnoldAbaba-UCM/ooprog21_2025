bfc64e0347baca57caea186b1d7b8650f0f89227
ArnoldAbaba-UCM <gmfireababa@gmail.com>
2025-09-16 08:49:11 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,16 @@
+public class Comments{
+   public static void main(String args[]){
+   //Single line comment
+   
+   /*block
+   line
+   comment*/
+   
+   /**
+      *Documentation Comment
+      *1
+      *2
+      *3
+      */
+   }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,12 @@
+public class FavoriteSong{
+   public static void main(String args[]){
+      System.out.println("I switched the time zone, but what do I know?");
+      System.out.println("Spendin' nights hitchhikin', where will I go?");
+      System.out.println("I could fly home, with my eyes closed");
+      System.out.println("But it'd get kinda hard to see, that's no surprise though");
+      System.out.println("And you could find me, I ain't hidin'");
+      System.out.println("I don't move my feet when I be glidin'");
+      System.out.println("I just slide in and then I roll out");
+      //ARNOLD A. ABABA
+   }
+}
