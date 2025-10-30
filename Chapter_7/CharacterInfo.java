33b8955219d203641683aedddf6a0e2aa61f6c78
ArnoldAbaba-UCM <gmfireababa@gmail.com>
2025-10-30 13:13:26 +0800

    Add files via upload

---
+++ Chapter_7/CharacterInfo.java
@@ -0,0 +1,36 @@
+//import java.util.Scanner;
+
+public class CharacterInfo{
+   public static void main (String args[]){
+      char charr;
+      String CASE = "";
+      //Scanner scan = new Scanner(System.in);
+      charr = 'C';
+      System.out.print("The character is ");
+      //char charr = scan.next().charAt(0);
+      
+      if(Character.isLowerCase(charr)==true){
+         System.out.println(charr+" is lowercase");
+         CASE = "uppercase";
+      }else{
+         System.out.println(charr+" is uppercase");
+         CASE = "lowercase";
+      }
+      
+      System.out.println(charr+" is not "+CASE);
+      System.out.println("After toLowerCase(), aChar is "+Character.toLowerCase(charr));
+      System.out.println("After toUpperCase(), aChar is "+Character.toUpperCase(charr));
+      
+      if(Character.isLetterOrDigit(charr)==true){
+         System.out.println(charr+" is a letter or digit");
+      }else{
+         System.out.println(charr+"not a letter or digit");
+      }
+      
+      if(Character.isWhitespace(charr)==true){
+         System.out.println(charr+" is a whitespace");
+      }else{
+         System.out.println(charr+" is not a whitespace");
+      }
+   }
+}
